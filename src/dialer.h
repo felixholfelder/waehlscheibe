2bdd492147333159c08ec0d2e6b1027bde200a0a
Felix Holfelder <felixholfelder1@t-online.de>
2026-09-15 17:35:34 +0200

    dialer as own class

---
+++ src/dialer.h
@@ -0,0 +1,73 @@
+#ifndef DIALER_H
+#define DIALER_H
+
+#include <Arduino.h>
+
+class Dialer {
+public:
+    Dialer(const uint8_t workingPin, const uint8_t countingPin, unsigned long debounceMs = 5)
+        : _workingPin(workingPin)
+        , _countingPin(countingPin)
+        , _debounceDelay(debounceMs)
+    {}
+
+    void begin() const {
+        pinMode(_workingPin, INPUT_PULLUP);
+        pinMode(_countingPin, INPUT_PULLUP);
+    }
+
+    bool update() {
+        if (isWorking()) {
+            if (!_wasWorking) {
+                // Wahlvorgang beginnt
+                _counter = 0;
+                _lastCountingState = isCounting();
+                _wasWorking = true;
+            }
+
+            bool currentCountingState = isCounting();
+            if (currentCountingState != _lastCountingState) {
+                unsigned long now = millis();
+                if (now - _lastDebounceTime > _debounceDelay) {
+                    _lastDebounceTime = now;
+                    if (currentCountingState) {
+                        _counter++;
+                    }
+                    _lastCountingState = currentCountingState;
+                }
+            }
+            return false;
+        }
+
+        if (_wasWorking) {
+            _wasWorking = false;
+            return true;
+        }
+
+        return false;
+    }
+
+    int getCount() const {
+        return _counter;
+    }
+
+private:
+    bool isWorking() const {
+        return digitalRead(_workingPin) == LOW;
+    }
+
+    bool isCounting() const {
+        return digitalRead(_countingPin) == HIGH;
+    }
+
+    uint8_t _workingPin;
+    uint8_t _countingPin;
+    unsigned long _debounceDelay;
+
+    bool _wasWorking = false;
+    bool _lastCountingState = false;
+    unsigned long _lastDebounceTime = 0;
+    int _counter = 0;
+};
+
+#endif // DIALER_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,9 @@
 #include <Arduino.h>
 #include <Keyboard.h>
 #include "dial_config.h"
+#include "dialer.h"
 
-const int WORKING_PIN = 9;
-const int COUNTING_PIN = 10;
-
-int working_state = 0;
-int counter = 0;
-
-bool lastCountingState = false;
-unsigned long lastDebounceTime = 0;
-const unsigned long debounceDelay = 5;
-
-bool is_dial_working() {
-    return digitalRead(WORKING_PIN) == LOW;
-}
-
-bool is_dial_counting() {
-    return digitalRead(COUNTING_PIN) == HIGH;
-}
+static Dialer dialer(9, 10);
 
 int pulses_to_digit(int pulseCount) {
     if (pulseCount < 1 || pulseCount > 10) return -1;
@@ -33,44 +18,23 @@ const char* digit_to_string(int digit) {
 void setup() {
     Serial.begin(9600);
     Serial1.begin(9600);
-    pinMode(WORKING_PIN, INPUT_PULLUP);
-    pinMode(COUNTING_PIN, INPUT_PULLUP);
 
+    dialer.begin();
     Keyboard.begin(KEYBOARD_LAYOUT);
 }
 
 void loop() {
-    while (is_dial_working()) {
-        if (working_state == 0) {
-            working_state = 1;
-            counter = 0;
-            lastCountingState = is_dial_counting();
-        }
-
-        bool currentCountingState = is_dial_counting();
-        if (currentCountingState != lastCountingState) {
-            unsigned long now = millis();
-            if (now - lastDebounceTime > debounceDelay) {
-                lastDebounceTime = now;
-                if (currentCountingState) counter++;
-                lastCountingState = currentCountingState;
-            }
-        }
-    }
-
-    if (working_state == 1) {
-        int digit = pulses_to_digit(counter);
-        const char* text = digit_to_string(digit);
+    if (dialer.update()) {
+        int digit = pulses_to_digit(dialer.getCount());
 
         if (USE_DIGIT_STRINGS) {
+            const char* text = digit_to_string(digit);
             Serial.println(text);
             Keyboard.print(text);
         } else {
             Serial.println(digit);
             Keyboard.print(digit);
         }
-
-        working_state = 0;
     }
 
     delay(20);
