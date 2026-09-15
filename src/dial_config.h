f7dd738509bdfb3b5281cd1574d01856a2411cd4
Felix Holfelder <felixholfelder1@t-online.de>
2026-09-15 17:18:16 +0200

    added base setup to print to serial and write digits/sequences

---
+++ src/dial_config.h
@@ -0,0 +1,21 @@
+#ifndef DIAL_CONFIG_H
+#define DIAL_CONFIG_H
+
+const int DIAL_MAP[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+const char* const DIGIT_STRINGS[10] = {
+    "Harald ", // 0
+    "a ", // 1
+    "Zwei", // 2
+    "Drei", // 3
+    "Vier", // 4
+    "Matz ", // 5
+    "Sechs", // 6
+    "Sieben", // 7
+    "is ", // 8
+    "Neun" // 9
+};
+
+const bool USE_DIGIT_STRINGS = true;
+
+#endif // DIAL_CONFIG_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,77 @@
 #include <Arduino.h>
+#include <Keyboard.h>
+#include "dial_config.h"
 
-#define LED_PIN 13
-#define SPEED 1
+const int WORKING_PIN = 9;
+const int COUNTING_PIN = 10;
 
-void blink() {
-    digitalWrite(LED_PIN, HIGH);
-    delay(100 * SPEED);
-    digitalWrite(LED_PIN, LOW);
+int working_state = 0;
+int counter = 0;
+
+bool lastCountingState = false;
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 5;
+
+bool is_dial_working() {
+    return digitalRead(WORKING_PIN) == LOW;
+}
+
+bool is_dial_counting() {
+    return digitalRead(COUNTING_PIN) == HIGH;
+}
+
+int pulses_to_digit(int pulseCount) {
+    if (pulseCount < 1 || pulseCount > 10) return -1;
+    return DIAL_MAP[pulseCount - 1];
+}
+
+const char* digit_to_string(int digit) {
+    if (digit < 0 || digit > 9) return "";
+    return DIGIT_STRINGS[digit];
 }
 
 void setup() {
-    Serial.begin(115200);
-    pinMode(LED_PIN, OUTPUT);
+    Serial.begin(9600);
+    Serial1.begin(9600);
+    pinMode(WORKING_PIN, INPUT_PULLUP);
+    pinMode(COUNTING_PIN, INPUT_PULLUP);
+
+    Keyboard.begin(KeyboardLayout_de_DE);
 }
 
 void loop() {
-    blink();
-    delay(200);
+    while (is_dial_working()) {
+        if (working_state == 0) {
+            working_state = 1;
+            counter = 0;
+            lastCountingState = is_dial_counting();
+        }
+
+        bool currentCountingState = is_dial_counting();
+        if (currentCountingState != lastCountingState) {
+            unsigned long now = millis();
+            if (now - lastDebounceTime > debounceDelay) {
+                lastDebounceTime = now;
+                if (currentCountingState) counter++;
+                lastCountingState = currentCountingState;
+            }
+        }
+    }
+
+    if (working_state == 1) {
+        int digit = pulses_to_digit(counter);
+        const char* text = digit_to_string(digit);
+
+        if (USE_DIGIT_STRINGS) {
+            Serial.println(text);
+            Keyboard.print(text);
+        } else {
+            Serial.println(digit);
+            Keyboard.print(digit);
+        }
+
+        working_state = 0;
+    }
+
+    delay(20);
 }
